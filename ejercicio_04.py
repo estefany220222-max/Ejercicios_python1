84c8a7355b29181d9c4d45d447782831a117a13d
estefany220222-max <estefany220222@gmail.com>
2026-01-23 10:34:02 -0600

    Mi primer commit

---
+++ ejercicio_04.py
@@ -0,0 +1,14 @@
+#Dados dos nùmeros dmuestra la suma,
+#resta, multiplicacion y division
+num_1 = int(input('Ingresa un numero: '))
+num_2 = int(input('Ingresa otro numero: '))
+
+suma = num_1 + num_2
+resta = num_1 - num_2
+multi = num_1 * num_2
+divi = num_1 / num_2
+
+print('Suma:', suma)
+print('resta:', resta)
+print('multiplicacion:', multi)
+print('Division:', divi)
---
+++ ejercicio_05.py
@@ -0,0 +1,7 @@
+#Convertir grados Fahrenheit a celcius
+fahrenheit = float(input('Ingresar los grados Fahrenheit: '))
+
+celsius = (fahrenheit - 32) * (5/9)
+
+print('Fahrenheit', fahrenheit)
+print('Celsius', celsius)
