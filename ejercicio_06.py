b80e5f73a24ab9a05f4f6036f26db55c0071db47
estefany220222-max <estefany220222@gmail.com>
2026-01-28 16:02:43 -0600

    ejercicio condicional parte 2

---
+++ ejercicio_06.py
@@ -0,0 +1,6 @@
+'''Programa que lee una letra y dice si es mayuscula'''
+letter = input('Ingresa una letra: ')
+if letter == letter.upper():
+	print('Es una letra mayuscula')
+else:
+	print('No es una letra mayuscula')
